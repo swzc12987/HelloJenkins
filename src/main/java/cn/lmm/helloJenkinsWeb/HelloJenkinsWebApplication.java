3d2ccaa89deccdd64aecca34910f92b8b483a536
zhangchi <zhangchi@hzcoding.com>
2018-02-26 15:32:32 +0800

    https://github.com/swzc12987/HelloJenkins.git

---
+++ src/main/java/cn/lmm/helloJenkinsWeb/HelloJenkinsWebApplication.java
@@ -0,0 +1,12 @@
+package cn.lmm.helloJenkinsWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsWebApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/lmm/helloJenkinsWeb/HelloJenkinsWebApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.lmm.helloJenkinsWeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloJenkinsWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
