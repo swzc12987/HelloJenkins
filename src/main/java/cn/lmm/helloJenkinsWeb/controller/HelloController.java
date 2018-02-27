2487202eec26c522075e9b0100cb704e14c68cb7
zhangchi <zhangchi@hzcoding.com>
2018-02-27 14:38:57 +0800

    111

--- src/main/java/cn/lmm/helloJenkinsWeb/controller/HelloController.java
+++ src/main/java/cn/lmm/helloJenkinsWeb/controller/HelloController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController{
 	
 	@RequestMapping("hello")
 	public String hello() {
 		return "Hello Jenkins";
 	}
+	
+	
 }
